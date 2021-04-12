c5e387fd7ebf748a2e004d466e65ad531afc2b69
julia_hofmeister@taylor.edu <juhofmei@cse21810.cs.local>
2021-04-12 08:54:12 -0400

    added commments

--- badLogic/physicsEngine.cpp
+++ badLogic/physicsEngine.cpp
@@ -1,1 +1,13 @@
 //do physics
+#include <SFML/Graphics.hpp>
+#include <algorithm>
+#include <sstream>
+#include <unistd.h>
+
+using namespace sf;
+
+//gravity
+//applying forces and pulses
+//collision
+//direction
+
