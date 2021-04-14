1213a11f299a293e6430f5a7b8ca6442c46fb38a
julia_hofmeister@taylor.edu <juhofmei@cse21810.cs.local>
2021-04-14 09:04:15 -0400

    added gravity and basic verlet method

--- badLogic/physicsEngine.cpp
+++ badLogic/physicsEngine.cpp
@@ -7,7 +7,18 @@
 using namespace sf;
 
 //gravity
-//applying forces and pulses
+Vector2 gravity=Vector2(0,-9.8);
+
+//position
+verlet(obj){
+	obj.position = obj.position+obj.velocity*changeT+
+		0.5*obj.acceleration*changeT^2;
+	halfV = obj.velocity+0.5*obj.acceleration*changeT;
+	//change acceleration??
+	obj.velocity = halfV+0.5*obj.acceleration*changeT;
+}
+
+//applying forces and pulses-spring mass
 //collision
 //direction
 
