e51c9306248d32355241186b5a9be8598faa3870
Connor White <62063649+cqwhite@users.noreply.github.com>
2021-04-29 12:44:27 -0400

    Update game.cpp

--- badLogic/game.cpp
+++ badLogic/game.cpp
@@ -9,8 +9,6 @@
 #include "physicsEngine.h"
 
 /*things to do still
-        fix corner collisions
-        force transfer between objects
         check all collisions through a list
         buttons?
         wind?
@@ -18,53 +16,91 @@
 
 using namespace sf;
 
+std::string resources(){
+        return "/home/CS/users/cwhite/.linux/COS370/370Final/badLogic/resources/";
+}
+
 int main()
-{
-    
+{   
     Clock clock;
+
+    
 
     Collider ball;
     ball.shape=0;
-    ball.x=90.0;
-    ball.y=100.0;
+    ball.x=40.0;
+    ball.y=250.0;
     ball.radius=20.0;
     ball.radius2=0.0; 
     ball.isStatic=false;
     ball.tempStatic=false;
     ball.velocity_y = 1.0;
-    ball.velocity_x = 0.5;
+    ball.velocity_x = 1.0;
     ball.acceleration_y = gravity;
-    ball.acceleration_x = 3.0;
+    ball.acceleration_x = 50.0;
     ball.mass = 5.0;
     ball.material = "rubber";
 
+    Collider wall1;
+    wall1.shape=0;
+    wall1.x=200.0;
+    wall1.y=289.0;
+    wall1.radius=20.0;
+    wall1.radius2=40.0;
+    wall1.isStatic=false;
+    wall1.tempStatic=false;
+    wall1.velocity_y = 0.0;
+    wall1.velocity_x = 0.0;
+    wall1.acceleration_y = 0;
+    wall1.acceleration_x = 0.0;
+    wall1.mass = 4.0;
+    wall1.material = "plastic";
+
+    Collider wall2;
+    wall2.shape=0;
+    wall2.x=200.0;
+    wall2.y=331.0;
+    wall2.radius=20.0;
+    wall2.radius2=40.0;
+    wall2.isStatic=false;
+    wall2.tempStatic=false;
+    wall2.velocity_y = 0.0;
+    wall2.velocity_x = 0.0;
+    wall2.acceleration_y = 0;
+    wall2.acceleration_x = 0.0;
+    wall2.mass = 4.0;
+    wall2.material = "plastic";
+
+    Collider wall3;
+    wall3.shape=0;
+    wall3.x=241.0;
+    wall3.y=289.0;
+    wall3.radius=20.0;
+    wall3.radius2=40.0;
+    wall3.isStatic=false;
+    wall3.tempStatic=false;
+    wall3.velocity_y = 0.0;
+    wall3.velocity_x = 0.0;
+    wall3.acceleration_y = 0;
+    wall3.acceleration_x = 0.0;
+    wall3.mass = 4.0;
+    wall3.material = "plastic";
+
+
     Collider ball2;
     ball2.shape=0;
-    ball2.x=200.0;
-    ball2.y=100.0;
+    ball2.x=275.0;
+    ball2.y=325.0;
     ball2.radius=20.0;
+    ball2.radius2=0.0; 
     ball2.isStatic=false;
-    ball.tempStatic=false;
-    ball2.velocity_y = 1.0;
-    ball2.velocity_x = -1.0;
-    ball2.acceleration_y = gravity;
-    ball2.acceleration_x = -5.0;
+    ball2.tempStatic=false;
+    ball2.velocity_y = 0.0;
+    ball2.velocity_x = 0.0;
+    ball2.acceleration_y = 0;
+    ball2.acceleration_x = 0.0;
     ball2.mass = 6.0;
-    ball2.material = "plastic";
-
-    Collider ball3;
-    ball3.shape=0;
-    ball3.x=100.0;
-    ball3.y=150.0;
-    ball3.radius=10.0;
-    ball3.isStatic=false;
-    ball.tempStatic=false;
-    ball3.velocity_y = 1.0;
-    ball3.velocity_x = -1.0;
-    ball3.acceleration_y = gravity;
-    ball3.acceleration_x = -1.0;
-    ball3.mass = 6.0;
-    ball3.material = "plastic";
+    ball2.material = "rubber";
 
     Collider ground;
     ground.shape=1;
@@ -76,16 +112,6 @@ int main()
     ground.mass = 50.0;
     ground.material = "hard";
 
-    Collider ledge;
-    ledge.shape=1;
-    ledge.x=160.0;
-    ledge.y=300.0;
-    ledge.radius=80.0;
-    ledge.radius2=10.0;
-    ledge.isStatic=true;
-    ledge.mass = 20.0;
-    ledge.material = "sticky";
-
     Collider ball4;
     ball4.shape=0;
     ball4.x=0.0;
@@ -93,7 +119,7 @@ int main()
     ball4.radius=20.0;
     ball4.radius2=0.0; 
     ball4.isStatic=false;
-    ball.tempStatic=false;
+    ball4.tempStatic=false;
     ball4.velocity_y = 40.0;
     ball4.velocity_x = 60.0;
     ball4.acceleration_y = gravity;
@@ -111,7 +137,24 @@ int main()
     box.mass=50;
     box.material="hard";
     
-    RenderWindow window(VideoMode(400, 400), "Gravity Demo");
+    RenderWindow window(VideoMode(400, 400), "Geometry Crash");
+
+    Font font;
+    if(!font.loadFromFile(resources() + "ubuntu.ttf")){
+            std::cout<<"Error"<<std::endl;
+    }
+
+    font.loadFromFile(resources() + "ubuntu.ttf");
+
+    Text menuText;
+
+    menuText.setFont(font);
+    menuText.setCharacterSize(40);
+    menuText.setPosition(100.f, 200.f);
+    menuText.setFillColor(Color::White);
+
+    menuText.setString("Winner!");
+    
 
     //Colors: gray=metal; green=rubber; red=plastic; orange=sticky
     Color gray(100,100,100);
@@ -122,20 +165,25 @@ int main()
     player.setRadius(ball.radius); 
     player.setFillColor(Color::Green);
 
-    CircleShape player2;
-    player2.setPosition(ball2.x, ball2.y);
-    player2.setRadius(ball2.radius);
-    player2.setFillColor(Color::Red);
+    CircleShape enemy;
+    enemy.setPosition(ball2.x, ball2.y);
+    enemy.setRadius(ball2.radius); 
+    enemy.setFillColor(orange);
 
-    CircleShape player3;
-    player3.setPosition(ball3.x, ball3.y);
-    player3.setRadius(ball3.radius);
-    player3.setFillColor(gray);
+    RectangleShape barrier1;
+    barrier1.setPosition(wall1.x, wall1.y);
+    barrier1.setSize(Vector2f(wall1.radius,wall1.radius2)); 
+    barrier1.setFillColor(Color::Red);
 
-    CircleShape player4;
-    player4.setPosition(ball4.x, ball4.y);
-    player4.setRadius(ball4.radius);
-    player4.setFillColor(Color::Blue);
+    RectangleShape barrier2;
+    barrier2.setPosition(wall2.x, wall2.y);
+    barrier2.setSize(Vector2f(wall2.radius,wall2.radius2)); 
+    barrier2.setFillColor(Color::Red);
+
+    RectangleShape barrier3;
+    barrier3.setPosition(wall3.x, wall3.y);
+    barrier3.setSize(Vector2f(wall3.radius,wall3.radius2)); 
+    barrier3.setFillColor(Color::Red);
 
 
     RectangleShape ground_1;
@@ -143,11 +191,7 @@ int main()
     ground_1.setSize(Vector2f(ground.radius,ground.radius2)); 
     ground_1.setFillColor(gray);
 
-    RectangleShape ledge_1;
-    ledge_1.setPosition(ledge.x, ledge.y);
-    ledge_1.setSize(Vector2f(ledge.radius,ledge.radius2)); 
-    ledge_1.setFillColor(orange);
-
+    
     RectangleShape box_1;
     box_1.setPosition(box.x, box.y);
     box_1.setSize(Vector2f(box.radius,box.radius2)); 
@@ -158,10 +202,12 @@ int main()
     point.setRadius(1);
     point.setFillColor(Color::Green);
 
-    Collider* colliders[] = {&ball, &ball2, &ball3, &ledge, &ground};
+    Collider* colliders[] = {&ball, &ball2, &wall1, &wall2, &wall3, &ground};
     
 
     int scene = 0;
+
+    int hit = 0;
 
     while (window.isOpen())
     {
@@ -175,45 +221,20 @@ int main()
         if(scene==0){
             
         
-                do_collisions(colliders, 5);
+                do_collisions(colliders, 6, hit);
 
-                /*if (collision(ball, ground)){
-                        collisions(ball,ground);
-                        velocity_cutoff(ball);
-                }
-                if (collision(ball2, ground)){
-                        collisions(ball2, ground);
-                        velocity_cutoff(ball2);
-                }
-                if (collision(ball3, ground)){
-                        collisions(ball3, ground);
-                        velocity_cutoff(ball3);
-                }
-                if (collision(ball2, ledge)){
-                        collisions(ball2, ledge);
-                        velocity_cutoff(ball2);
-                }
-                if(collision(ball2, ball)){
-                        collisions(ball2,ball);
-                        velocity_cutoff(ball2);
-                        velocity_cutoff(ball);
-                           
-                }
-                if(collision(ball, ball3)){
-                        collisions(ball,ball3);
-                        velocity_cutoff(ball3);
-                        velocity_cutoff(ball);
-                }*/
                 velocityUpdate(&ball, DT);
                 velocityUpdate(&ball2, DT);
-                velocityUpdate(&ball3, DT);
+                velocityUpdate(&wall1, DT);
+                velocityUpdate(&wall2, DT);
+                velocityUpdate(&wall3, DT);
+
+                if(hit > 2100){
+                        scene=1;
+                }
+                
         }
         if(scene==1){
-                if(collision(&ball4,&box)){
-                        collisions(&ball4,&box);
-                       
-                }
-                velocityUpdate(&ball4, DT); 
         }
 	
        
@@ -221,23 +242,23 @@ int main()
 
         if(scene==0){
                 player.setPosition(ball.x, ball.y);
-                player2.setPosition(ball2.x,ball2.y);
-                player3.setPosition(ball3.x,ball3.y);
+                enemy.setPosition(ball2.x, ball2.y);
+                barrier1.setPosition(wall1.x,wall1.y);
+                barrier2.setPosition(wall2.x,wall2.y);
+                barrier3.setPosition(wall3.x,wall3.y);
+                
                 window.clear();
                 window.draw(player);
-                window.draw(player2);
-                window.draw(player3);
+                window.draw(enemy);
+                window.draw(barrier1);
+                window.draw(barrier2);
+                window.draw(barrier3);
                 window.draw(ground_1);
-                window.draw(ledge_1);
                 window.display();
         }
         if(scene==1){
-                player4.setPosition(ball4.x,ball4.y);
-                point.setPosition(ball4.x+(ball4.radius), ball4.y+ball4.radius);
                 window.clear();
-                window.draw(player4);
-                window.draw(box_1);
-                window.draw(point);
+                window.draw(menuText);
                 window.display();
         } 
 	
